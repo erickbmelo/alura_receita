f25d10c88418230fd0994b8bc64cd30967d9042e
erick.melo <erick.melo@sorocaba.sp.gov.br>
2022-12-09 18:06:37 -0300

    Aula 2/3, adicionando tags static, extends

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from receitas.views import index
+from receitas.views import index, receita
 
 urlpatterns = [
         path('', index, name='index'),
+        path('receita', receita, name='receita')
 ]
+
+# Outra opção
+#from . import views
+#urlpatterns = [
+#        path('', views.index, name='index'),
+#        path('receita', views.receita, name='receita')
+#]
