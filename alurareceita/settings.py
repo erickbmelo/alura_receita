f25510a2b22b0b2633a513350260a05a4b70c481
erick.melo <erick.melo@sorocaba.sp.gov.br>
2022-12-12 16:26:04 -0300

    Aula 4. Conectando com banco. Criando classe/model.

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dtin_local',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '172.18.189.240'
     }
 }
 
