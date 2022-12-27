17d2cbc377262485e14070fc6f355980a2d8a633
erick.melo <erick.melo@sorocaba.sp.gov.br>
2022-12-27 17:46:48 -0300

    Salvando cadastro de usuários no banco

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
     if request.method == 'POST':
-        print('Usuário criado com sucesso')
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário cadastrado com sucesso')
+        print(nome, email, senha, senha2)
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
